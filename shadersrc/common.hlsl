cf6d885d293808e1087e2e5eeebc86d3cbb2d440
Michael-Soyka <45078459+Michael-Soyka@users.noreply.github.com>
2025-05-26 17:22:31 +0300

    feat: Added header for vertex shader

--- shadersrc/common.hlsl
+++ shadersrc/common.hlsl
@@ -1,31 +1,44 @@
-// common data shared among all screenspace shaders
+#ifndef __COMMON_HLSL__
+#define __COMMON_HLSL__
 
-// up to four textures available for sampling
-sampler TexBase : register( s0 ); // $basetexture
-sampler Tex1    : register( s1 ); // $texture1
-sampler Tex2    : register( s2 ); // $texture2
-sampler Tex3    : register( s3 ); // $texture3
+/* Texture samplers
+ *  $basetexture
+ *  $texture1
+ *  $texture2
+ *  $texture3
+ */
+sampler TexBase : register( s0 );
+sampler Tex1    : register( s1 );
+sampler Tex2    : register( s2 );
+sampler Tex3    : register( s3 ); 
 
-// normalized dimensions for each texture above
-// (x = 1.0 / width, y = 1.0 / height)
-// NOT AVAILABLE IN L4D2/PORTAL 2
+/* Texure's normalized dimensions
+ *  ( x = 1.0 / width, y = 1.0 / height )
+ *  WARNING: NOT AVAILABLE IN L4D2/PORTAL 2
+ */
 float2 TexBaseSize : register( c4 );
 float2 Tex1Size    : register( c5 );
 float2 Tex2Size    : register( c6 );
 float2 Tex3Size    : register( c7 );
 
-// customizable parameters $c0, $c1, $c2, $c3, $c4
+/* Custom params
+ *  $c0
+ *  $c1
+ *  $c2
+ *  $c3
+ *  $c4 - ONLY AVAILABLE IN L4D2/PORTAL2
+ */
 const float4 Constants0 : register( c0 );
 const float4 Constants1 : register( c1 );
 const float4 Constants2 : register( c2 );
 const float4 Constants3 : register( c3 );
-// ONLY AVAILABLE IN L4D2/PORTAL2 (
 const float4 Constants4 : register( c4 );
 
-// eye position in world coordinates
-// WARNING: this is calculated from the view matrix
-// on a screen overlay, this will return near-zero values
-// use PlayerPosition VMT proxy as a workaround
+/* Eye position in world coordinates
+ *  WARNING: this is calculated from the view matrix
+ *  on a screen overlay, this will return near-zero values
+ *  use PlayerPosition VMT proxy as a workaround
+ */
 const float4 EyePosition : register( c10 );
 
 // xyz will be 0, 0, 0 (black) if mesh is rendered without fog
@@ -43,20 +56,13 @@ const float4 HDRParams   : register( c30 );
 // gamma, equivalent to pow(TonemapScale, 1.0 / 2.2)
 #define GammaScale         HDRParams.w
 
-// interpolated vertex data from vertex shader, do not change
 struct PS_INPUT
 {
-	// texture coordinates
-	float2 uv		    : TEXCOORD0;
-	// always (0, 0)
-	float2 zeros        : TEXCOORD1;
-	// unused
-	float2 texcoord2    : TEXCOORD2;
-	// vertex color (if mesh has one)
-	float4 color		: TEXCOORD3;
-	// screenspace position
-	// SHADER MODEL 3 ONLY
-	float2 pos			: VPOS;
+    float2 pos          : VPOS;
+    float2 uv           : TEXCOORD0;
+    float4 color        : TEXCOORD1;
 };
 
 #include "functions.hlsl"
+
+#endif // __COMMON_HLSL__
